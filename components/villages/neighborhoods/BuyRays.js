fcc81c1cf9db5e8167d502376d53edda48303b29
Shahid Karimi <me@shahidkarimi.com>
2018-05-28 02:37:32 +0500

    replaced Visor with Rays

--- components/villages/neighborhoods/BuyRays.js
+++ components/villages/neighborhoods/BuyRays.js
@@ -9,7 +9,7 @@ class BuyRays extends React.Component {
       <section className="cta" id="buyRays">
         <div className="cta-content">
           <div className="container">
-            <h2>VisorPay allows vendors<br/>to accept XVR > USD, GBP & EUR payments</h2>
+            <h2>Private and Pre Sale information is coming soon</h2>
             <a href="#contact" className="btn btn-outline btn-xl js-scroll-trigger">Find out more</a>
           </div>
         </div>
